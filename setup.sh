59e55e3bea5b2ec4beabc5040bb90f738ae73441
Bora Semiz <borasemiz50@gmail.com>
2024-12-09 21:02:16 +0100

    replace ssh with https and use sudo

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@
 export XDG_CONFIG_HOME="$HOME/.config"
 mkdir -p "$XDG_CONFIG_HOME"
 
-apt update
-apt install ripgrep git lazygit curl tar
+sudo apt update
+sudo apt install ripgrep git lazygit curl tar
 
-git clone git@github.com:borasemiz/my-nvim.git "$XDG_CONFIG_HOME/nvim"
+git clone https://github.com/borasemiz/my-nvim.git "$XDG_CONFIG_HOME/nvim"
 
 curl -o "$HOME/nvim-linux64.tar.gz" https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
 tar xzvf "$HOME/nvim-linux64.tar.gz"
 
-echo "export PATH=$HOME/nvim-linux64/bin"
+echo "export PATH=$HOME/nvim-linux64/bin" >"$HOME/.bashrc"
