54c6566aba6778696dd9483022c964340fd2c7f9
Bora Semiz <borasemiz50@gmail.com>
2024-12-10 22:12:11 +0100

    append instead of overwrite

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ git clone https://github.com/borasemiz/my-nvim.git "$XDG_CONFIG_HOME/nvim"
 curl -o "$HOME/nvim-linux64.tar.gz" https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
 tar xzvf "$HOME/nvim-linux64.tar.gz"
 
-echo "export PATH=$HOME/nvim-linux64/bin" >"$HOME/.bashrc"
+echo "export PATH=$PATH:$HOME/nvim-linux64/bin" >>"$HOME/.bashrc"
