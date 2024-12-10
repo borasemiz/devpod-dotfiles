e1bc4089d8812a5411a0b0167b826757001636e5
Bora Semiz <borasemiz50@gmail.com>
2024-12-10 22:23:26 +0100

    something

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ export XDG_CONFIG_HOME="$HOME/.config"
 mkdir -p "$XDG_CONFIG_HOME"
 
 sudo apt update
-sudo apt install ripgrep git curl tar
+sudo apt install ripgrep git curl tar wget
 
 git clone https://github.com/borasemiz/my-nvim.git "$XDG_CONFIG_HOME/nvim"
 
-curl -o "$HOME/nvim-linux64.tar.gz" https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
+wget -O "$HOME/nvim-linux64.tar.gz" https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
 tar xzvf "$HOME/nvim-linux64.tar.gz"
 
 echo "export PATH=$PATH:$HOME/nvim-linux64/bin" >>"$HOME/.bashrc"
