07b7ca4385608153fc894766df32b09e716d33bd
Bora Semiz <borasemiz50@gmail.com>
2024-12-10 22:49:44 +0100

    something

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ git clone https://github.com/borasemiz/my-nvim.git "$XDG_CONFIG_HOME/nvim"
 wget -O "$HOME/nvim-linux64.tar.gz" https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
 echo $(pwd)
 mkdir -p "$HOME/nvim-linux64"
-tar xzf "$HOME/nvim-linux64.tar.gz" -C "$HOME/nvim-linux64"
+tar xzf "$HOME/nvim-linux64.tar.gz" -C "$HOME/"
 
 echo "export PATH=$PATH:$HOME/nvim-linux64/bin" >>"$HOME/.bashrc"
